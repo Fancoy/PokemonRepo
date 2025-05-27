3ed1e19f19588e7b0196b9205b95a2d9e91e21ef
Fancoy <emre.gurbuz@ug.bilkent.edu.tr>
2025-05-27 13:25:34 +0300

    New Update

--- src/app/pokemon/[pokemon_id]/page.tsx
+++ src/app/pokemon/[pokemon_id]/page.tsx
@@ -44,7 +44,7 @@ console.log(pokemon_id)
                 console.error(error);
                 setPokemonLoaded(true);
             });
-    }, []);
+    }, [pokemon_id]);
 
     return (
         <>
@@ -54,7 +54,7 @@ console.log(pokemon_id)
                     pokemon ?
                         <PokemonComponent pokemon={pokemon}></PokemonComponent> :
                         <Image className='img-fluid mx-auto d-block rounded'
-                            src="https://cdn.dribbble.com/users/2805817/screenshots/13206178/media/6bd36939f8a01d4480cb1e08147e20f3.png" /> :
+                            src="https://cdn.dribbble.com/users/2805817/screenshots/13206178/media/6bd36939f8a01d4480cb1e08147e20f3.png" alt= "pokemonImage" />  :
                     <Container>
                         <Row className="justify-content-md-center p-2">
                             <Spinner className='p-2' animation='border' role='status' />
